000924e56a7cf1f11e8a3885a1afdb02578a01eb
jjini-SUN <shineeseo@naver.com>
2024-05-26 10:43:51 +0900

    24.05.26 react_TS

---
+++ src/components/Profile/Profile.tsx
@@ -0,0 +1,15 @@
+//리액트 기반 컴포넌트는 .tsx로 생성
+//.ts는 리액트 컴포넌트 안됨 (jsx 문법 사용 불가)
+
+import { useState } from "react"
+
+export default function Profile(props : {name:string, age : string}) : JSX.Element {
+  //state는 일반적으로 타입을 명사 X (초기값 적용)
+  let [myState, setMyState] = useState(0);
+
+  return (
+    <>
+      <p>{props.name} 입니다.</p>
+    </>
+  )
+}
